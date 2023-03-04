8bb71c545122368dbf08ef3f1aecace687987fc8
Dorin Lazăr <dorin.lazar@gmail.com>
2023-03-04 22:49:34 +0200

    added int parser

--- src/customparsers.hpp
+++ src/customparsers.hpp
@@ -4,18 +4,37 @@
 #include <array>
 
 struct Parser {
-static constexpr bool is_digit (char c)  {
-  return (unsigned char)(c-'0')<10;
-}
+  static constexpr bool is_digit(char c) { return (unsigned char)(c - '0') < 10; }
 
-// template <typename T>
-// requires (std::is_integral<T>::value)
-// T parse(std::string_view value) noexcept {
-//   if (value.size()==0) {return 0;}
-//   size_t offset = 0;
-//   if constexpr (std::is_signed<T>::value) {
-//     if 
-//   }
-// }
-
+  template <typename T>
+    requires(std::is_integral<T>::value)
+  static constexpr T parse_int(std::string_view value) noexcept {
+    if (value.size() == 0) {
+      return 0;
+    }
+    size_t offset = 0;
+    bool negative = false;
+    if constexpr (std::is_signed<T>::value) {
+      if (value[0] == '-') {
+        negative = true;
+        offset++;
+      } else if (value[0] == '+') {
+        offset++;
+      }
+    }
+    T result = 0;
+    while (offset < value.size()) {
+      if (!is_digit(value[offset])) {
+        break;
+      }
+      result = result * 10 + value[offset] - '0';
+      offset++;
+    }
+    if constexpr (std::is_signed<T>::value) {
+      if (negative) {
+        return -result;
+      }
+    }
+    return result;
+  }
 };
--- src/tests/test-is-digit.cpp
+++ src/tests/test-is-digit.cpp
@@ -3,27 +3,28 @@
 #include <fmt/format.h>
 #include <chrono>
 
-int main()
-{
-  for (int c=0; c<256; c++) {
-    std::cout << fmt::format("{0:#04x} {0:08b} {1:>5}", c, Parser::is_digit(c)) <<std::endl;
+int main() {
+  for (int c = 0; c < 256; c++) {
+    std::cout << fmt::format("{0:#04x} {0:08b} {1:>5}", c, Parser::is_digit(c)) << std::endl;
   }
   size_t size = 100'000'000;
-  auto buffer= std::make_unique_for_overwrite<char[]>(size);
+  auto buffer = std::make_unique_for_overwrite<char[]>(size);
   size_t count1 = 0;
 
-  for (size_t i=0; i<size; i++) {
-    buffer[i] = (i*71)&0xFF;
+  for (size_t i = 0; i < size; i++) {
+    buffer[i] = (i * 71) & 0xFF;
   }
 
   auto start = std::chrono::system_clock::now();
-  for (size_t i=0; i<size; i++) {
+  for (size_t i = 0; i < size; i++) {
     count1 += Parser::is_digit(buffer[i]);
   }
-  
+
   auto end = std::chrono::system_clock::now();
 
-  auto nanos1 = std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
+  auto nanos1 = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
   std::cout << fmt::format("{}  --  {}", count1, nanos1) << std::endl;
+  std::cout << fmt::format("{}  --  {}", Parser::parse_int<uint32_t>("15542"), Parser::parse_int<int32_t>("-15542"))
+            << std::endl;
   return 0;
 }
